4c18d08da34ce4166f8092d41f94f9b25753cb5b
Sami Siraj <sami.siraj.s.n2004@gmail.com>
2026-08-06 02:22:24 +0530

    feat: add hyperparameter-tuned logistic regression (marginal gain), save best model via joblib

--- src/train.py
+++ src/train.py
@@ -1,7 +1,8 @@
 from sklearn.linear_model import LogisticRegression
 
 
-def train(X_train, y_train):
-    model = LogisticRegression(max_iter=1000)
+def train_logistic(X_train, y_train):
+    model = LogisticRegression(C=1000, max_iter=3000, penalty='l1',
+                   random_state=42, solver='liblinear')
     model.fit(X_train, y_train)
     return model
