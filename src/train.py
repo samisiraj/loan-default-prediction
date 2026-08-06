6cd8d87edf01193de7f3b12f6065f2f37adcbb93
Sami Siraj <sami.siraj.s.n2004@gmail.com>
2026-08-06 14:51:28 +0530

    feat: tune decision tree, val AUC 0.84 → 0.87

--- src/train.py
+++ src/train.py
@@ -1,8 +1,15 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
 
 
 def train_logistic(X_train, y_train):
     model = LogisticRegression(C=1000, max_iter=3000, penalty='l1',
                    random_state=42, solver='liblinear')
+    model.fit(X_train, y_train)
+    return model
+
+def train_tree(X_train, y_train):
+    model = DecisionTreeClassifier(ccp_alpha=6.639769763389794e-05,
+                       random_state=42)
     model.fit(X_train, y_train)
     return model
